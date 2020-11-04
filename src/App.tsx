682da20efd7291ecc1d8eaf6a8785c9511ee65bd
Feoktistov D.S <feoktistovdmitriy@gmail.com>
2020-11-04 20:32:41 +0700

    created useModals hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import useModals from "./hooks/useModals";
 
 function App() {
+  const doSomething = (e: React.MouseEvent, id: string) => {
+    alert(`Колбек выполнен на модальном окне с id = ${id}`);
+  };
+  const { Modals, createModal } = useModals(doSomething);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className="open-button" onClick={createModal}>
+        Создать окно
+      </button>
+      <Modals />
     </div>
   );
 }
---
+++ src/hooks/useModals/index.tsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+import Modal from "../../components/Modal";
+import "./styles.css";
+
+interface ModalProps {
+  id: string;
+  content: React.ReactElement | string;
+  cb: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>, id: string) => void;
+}
+
+const uuidv4 = (): string => {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c === "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+};
+
+const useModals = (
+  cb: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>, id: string) => void
+) => {
+  const [modalsProps, setModalsProps] = useState<ModalProps[]>([]);
+  const closeModal = (id: string): void => {
+    setModalsProps(modalsProps.filter((mProp) => mProp.id !== id));
+  };
+
+  useEffect(() => {
+    const closeLastModal = (): void => {
+      const res = modalsProps.slice(0, -1);
+      setModalsProps(res);
+    };
+    const handleKeyUp = (e: KeyboardEvent) => {
+      e.preventDefault();
+      if (e.keyCode === 27) {
+        closeLastModal();
+      }
+    };
+    window.addEventListener("keyup", handleKeyUp);
+    return () => window.removeEventListener("keyup", handleKeyUp);
+  }, [modalsProps]);
+
+  const createModal = (): void => {
+    const id = uuidv4();
+    setModalsProps([
+      ...modalsProps,
+      { cb, id, content: `Контент модального окна c id: ${id}` },
+    ]);
+  };
+
+  const Modals: React.FC = () => {
+    return (
+      <div className="modals">
+        {modalsProps.map((modalProp) => (
+          <Modal {...modalProp} closeModal={closeModal} key={modalProp.id} />
+        ))}
+      </div>
+    );
+  };
+
+  return { Modals, createModal };
+};
+
+export default useModals;
